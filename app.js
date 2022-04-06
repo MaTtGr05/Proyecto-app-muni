abad92826a89429fd2dc5308a648283056177796
matias <mati.bleach16@gmail.com>
2022-04-06 09:59:14 -0400

    agregue un cambio, con el creador de la pagina

--- app.js
+++ app.js
@@ -2,28 +2,24 @@ const express = require ('express');
 const res = require('express/lib/response');
 const app = express();
 
-app.get('/', (req, res)=>{
+app.get('/Homescreen',(req, res)=>{
     res.sendFile("C:/Users/Usuario/Desktop/Proyecto-app/index.html");
 });
 
-app.get('/Homescreen',(req, res)=>{
-    res.sendFile("C:/Users/Usuario/Desktop/Proyecto-app/index2.html");
-});
-
 app.get('/FormContacto',(req, res)=>{
-    res.sendFile("C:/Users/Usuario/Desktop/Proyecto-app/index3.html");
+    res.sendFile("C:/Users/Usuario/Desktop/Proyecto-app/index2.html");
 })
 
 app.get('/ConocelosP',(req, res)=>{
-    res.sendFile("C:/Users/Usuario/Desktop/Proyecto-app/index4.html");
+    res.sendFile("C:/Users/Usuario/Desktop/Proyecto-app/index3.html");
 })
 
 app.get('/tramitesDOM',(req, res)=>{
-    res.sendFile("C:/Users/Usuario/Desktop/Proyecto-app/index5.html");
+    res.sendFile("C:/Users/Usuario/Desktop/Proyecto-app/index4.html");
 })
 
 app.get('/Eventos',(req, res)=>{
-    res.sendFile("C:/Users/Usuario/Desktop/Proyecto-app/index6.html");
+    res.sendFile("C:/Users/Usuario/Desktop/Proyecto-app/index5.html");
 })
 
 app.listen(5500, (req, res)=>{
