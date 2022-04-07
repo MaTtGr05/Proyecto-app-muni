8abe5b3d19cdb78caa1219ec4acc0ec5a709863b
matias <mati.bleach16@gmail.com>
2022-04-07 17:22:42 -0400

    agregue un cambio, con el creador de la pagina

--- send.php
+++ send.php
@@ -1,8 +1,8 @@
 <?php
-$name = $_POST['nombre'];
+$name = $_POST['name'];
 $mail = $_POST['mail'];
-$phone = $_POST['telefono_contacto'];
-$message = $_POST['mensaje'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
 
 $header = 'From: ' . $mail . " \r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
@@ -12,12 +12,13 @@ $header .= "Content-Type: text/plain";
 $message = "Este mensaje fue enviado por: " . $name . " \r\n";
 $message .= "Su e-mail es: " . $mail . " \r\n";
 $message .= "Teléfono de contacto: " . $phone . " \r\n";
-$message .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
+$message .= "Mensaje: " . $_POST['message'] . " \r\n";
 $message .= "Enviado el: " . date('d/m/Y', time());
 
 $para = 'mati.bleach16@gmail.com';
-$asunto = 'Mensaje de '+$name;
+$asunto = 'Correo de prueba';
 
 mail($para, $asunto, utf8_decode($message), $header);
 
 header("Location:index.html");
+?>
