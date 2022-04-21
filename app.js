ead140b70d0afc7936506637b9238d1ef415a5b8
matias <mati.bleach16@gmail.com>
2022-04-21 10:51:39 -0400

    agregue un cambio

--- app.js
+++ app.js
@@ -2,26 +2,6 @@ const express = require ('express');
 const res = require('express/lib/response');
 const app = express();
 
-app.get('/Homescreen',(req, res)=>{
-    res.sendFile("C:/Users/Usuario/Desktop/Proyecto-app/index.html");
-});
-
-app.get('/FormContacto',(req, res)=>{
-    res.sendFile("C:/Users/Usuario/Desktop/Proyecto-app/index2.html");
-})
-
-app.get('/ConocelosP',(req, res)=>{
-    res.sendFile("C:/Users/Usuario/Desktop/Proyecto-app/index3.html");
-})
-
-app.get('/tramitesDOM',(req, res)=>{
-    res.sendFile("C:/Users/Usuario/Desktop/Proyecto-app/index4.html");
-})
-
-app.get('/Eventos',(req, res)=>{
-    res.sendFile("C:/Users/Usuario/Desktop/Proyecto-app/index5.html");
-})
-
 app.listen(5500, (req, res)=>{
     console.log('server inciado en el puerto 5500');
 });
